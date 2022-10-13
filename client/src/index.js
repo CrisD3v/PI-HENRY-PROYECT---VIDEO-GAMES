f28fedc268c4231d9ee6b2d1f66b2803ab18cf3e
CrisD3v <crisdevenginer@gmail.com>
2022-10-13 10:26:56 -0500

    fracture

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import axios from 'axios'
 import dotenv from "dotenv";
 dotenv.config();
-axios.defaults.baseURL =  "http://localhost:3001";
+axios.defaults.baseURL = process.env.REACT_APP_API ||"http://localhost:3001";
 
 ReactDOM.render(
   <Provider store={store}>
